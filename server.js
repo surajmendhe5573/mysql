a43be021ba1c4c94949ca58648b2bdcd0ecf9733
surajmendhe5573 <surajmendhe470@gmail.com>
2025-01-24 21:34:05 +0530

    changes

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const app = express();
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-// Middleware
 app.use(bodyParser.json());
 
-// Routes
 app.use('/api/users', require('./routes/user'));
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
